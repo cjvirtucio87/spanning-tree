1dbe456bfea0f7c434d02ddabb4b49a20fc8e446
CJ Virtucio <cjv287@gmail.com>
2021-10-03 15:24:36 -0400

    completed basic test

--- spanning_tree/tree.py
+++ spanning_tree/tree.py
@@ -3,7 +3,11 @@ Spanning Tree module.
 """
 
 import random
-from typing import List
+
+# traditional ephemeral port range
+# see: https://datatracker.ietf.org/doc/html/rfc6056#section-2.1
+_EPHEMERAL_PORT_MAX = 65535
+_EPHEMERAL_PORT_MIN = 1024
 
 def _ephemeral_port():
     """
@@ -11,7 +15,16 @@ def _ephemeral_port():
 
     returns: a generated ephemeral port
     """
-    return random.randrange(1024, 65535)
+    return random.randrange(_EPHEMERAL_PORT_MIN, _EPHEMERAL_PORT_MAX)
+
+def _traverse(bridge: 'Bridge', path, paths):
+    path.append(bridge)
+    if not bridge.listened:
+        paths.append(path.copy())
+        return
+
+    for listened in bridge.listened:
+        _traverse(listened, path.copy(), paths)
 
 class Port:
     """
@@ -19,8 +32,63 @@ class Port:
     """
 
     def __init__(self, number: int = None, priority: int = 1):
+        if number and (number < _EPHEMERAL_PORT_MIN or number > _EPHEMERAL_PORT_MAX):
+            raise ValueError("Outside port range.")
+
         self._number = number if number else _ephemeral_port()
         self._priority = priority
+        self._listener = None
+        self._listened = None
+        self._is_root = False
+
+    def __eq__(self, other: 'Port'):
+        return self.number == other.number
+
+    def connect(self, other: 'Port'):
+        """
+        Connect this port to another one.
+        """
+        other.listener = self
+        self.listened = other
+
+    @property
+    def connected(self):
+        """
+        Whether this port is connected to another port.
+        """
+        return self.listener is not None and self.listened is not None
+
+    @property
+    def listener(self):
+        """
+        Get the Port listening on this port.
+
+        returns: the Port listening on this port.
+        """
+        return self._listener
+
+    @listener.setter
+    def listener(self, listener: 'Port'):
+        """
+        Set the Port to listen on this port.
+        """
+        self._listener = listener
+
+    @property
+    def listened(self):
+        """
+        Get the Port that this port is listening to.
+
+        returns: the Port listened to by this port.
+        """
+        return self._listened
+
+    @listened.setter
+    def listened(self, listened: 'Port'):
+        """
+        Set the Port that this port is listening to.
+        """
+        self._listened = listened
 
     @property
     def is_root(self):
@@ -29,7 +97,14 @@ class Port:
 
         returns: whether the port is the root port.
         """
-        return False
+        return self._is_root
+
+    @is_root.setter
+    def is_root(self, is_root: bool):
+        """
+        Set the flag for whether the port is the root port.
+        """
+        self._is_root = is_root
 
     @property
     def number(self):
@@ -45,10 +120,11 @@ class Bridge:
     A bridge.
     """
 
-    def __init__(self, name: str, ports: List[Port] = None):
+    def __init__(self, name: str, *ports: Port):
         self._name = name
         self._ports = ports if ports else [Port()]
         self._is_root = False
+        self._listened = {}
 
     def __eq__(self, other: object):
         return isinstance(other, Bridge) and self.name == other.name
@@ -58,6 +134,17 @@ class Bridge:
 
     def __str__(self):
         return f"Bridge: {self.name}"
+
+    def connect(self, other: 'Bridge'):
+        """
+        Connect to another bridge.
+        """
+        unallocated_port = self._get_unallocated_port()
+        if not unallocated_port:
+            raise ValueError("No free ports.")
+
+        other.connect_port(unallocated_port)
+        self._listened[unallocated_port.number] = other
 
     def check_ports(self):
         """
@@ -71,6 +158,13 @@ class Bridge:
 
         self._is_root = False
 
+    def _get_unallocated_port(self):
+        for port in self._ports:
+            if not port.connected:
+                return port
+
+        return None
+
     @property
     def is_root(self):
         """
@@ -79,6 +173,25 @@ class Bridge:
         returns: whether the bridge is the root bridge.
         """
         return self._is_root
+
+    def connect_port(self, port: Port):
+        """
+        Connect port to the bridge's own unallocated port.
+        """
+        unallocated_port = self._get_unallocated_port()
+        if unallocated_port is None:
+            raise ValueError("No free ports.")
+
+        port.connect(unallocated_port)
+
+    @property
+    def listened(self):
+        """
+        Get all the bridges that this bridge is listening to.
+
+        returns: all the bridges that this bridge is listening tos.
+        """
+        return self._listened.values()
 
     @property
     def name(self):
@@ -108,5 +221,22 @@ class Tree: # pylint: disable=too-few-public-methods
         if self._shortest_path:
             return self._shortest_path
 
-        self._shortest_path = self._bridges
+        root_bridge = None
+        for bridge in self._bridges:
+            bridge.check_ports()
+            if bridge.is_root:
+                root_bridge = bridge
+                break
+
+        if not root_bridge:
+            raise ValueError("No root bridge in tree.")
+
+        for listened in root_bridge.listened:
+            paths = []
+            _traverse(listened, [root_bridge], paths)
+            paths.sort(key=len)
+
+            if self._shortest_path is None or (len(paths[0]) < len(self._shortest_path)):
+                self._shortest_path = paths[0]
+
         return self._shortest_path
--- tests/unit/test_tree.py
+++ tests/unit/test_tree.py
@@ -2,18 +2,36 @@
 
 import spanning_tree.tree as spanning_tree_tree
 
-# root <- a(1) <- c(1) <- d(1)
-#      <- b(1) <- e(1)
+# root <- a(1) <- c(1) <- d(1) <- f(1)
+#              <- b(1) <- e(1)
 #
 # Shortest path is:
 # root <- b <- e
 def test_tree_shortest_path_least_bridges():
-    root_bridge = spanning_tree_tree.Bridge('root')
-    bridge_a = spanning_tree_tree.Bridge('a')
+    root_port = spanning_tree_tree.Port(1024)
+    root_port.is_root = True
+    root_bridge = spanning_tree_tree.Bridge(
+        'root',
+        root_port)
+
+    bridge_a = spanning_tree_tree.Bridge(
+        'a',
+        spanning_tree_tree.Port(1024),
+        spanning_tree_tree.Port(1025))
     bridge_b = spanning_tree_tree.Bridge('b')
     bridge_c = spanning_tree_tree.Bridge('c')
     bridge_d = spanning_tree_tree.Bridge('d')
     bridge_e = spanning_tree_tree.Bridge('e')
+    bridge_f = spanning_tree_tree.Bridge('f')
+
+    root_bridge.connect(bridge_a)
+    bridge_a.connect(bridge_c)
+    bridge_c.connect(bridge_d)
+    bridge_d.connect(bridge_f)
+
+    bridge_a.connect(bridge_b)
+    bridge_b.connect(bridge_e)
+
     tree = spanning_tree_tree.Tree(
         root_bridge,
         bridge_a,
@@ -22,4 +40,4 @@ def test_tree_shortest_path_least_bridges():
         bridge_d,
         bridge_e)
 
-    assert [root_bridge, bridge_b, bridge_e] == tree.shortest_path()
+    assert [root_bridge, bridge_a, bridge_b, bridge_e] == tree.shortest_path()
